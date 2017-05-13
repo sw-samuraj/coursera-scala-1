c949e9a82abdf2bc99d32a6043f87d4e9ec9f991
Vít Kotačka <vit.kotacka@gmail.com>
2017-05-13 18:06:57 +0200

    Code formatting.

--- objsets/src/test/scala/objsets/TweetSetSuite.scala
+++ objsets/src/test/scala/objsets/TweetSetSuite.scala
@@ -8,6 +8,7 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class TweetSetSuite extends FunSuite {
+
   trait TestSets {
     val set1 = new Empty
     val set2 = set1.incl(new Tweet("a", "a body", 20))
@@ -71,4 +72,4 @@ class TweetSetSuite extends FunSuite {
     }
   }
 
-  }
+}
