b54fb88d41d6e7d73e2cc0ae12fc1762f6ef1aa6
Vít Kotačka <vit.kotacka@gmail.com>
2017-06-02 21:11:17 +0200

    Implementation of the 'until' function.

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -137,7 +137,12 @@ object Huffman {
     * the example invocation. Also define the return type of the `until` function.
     *  - try to find sensible parameter names for `xxx`, `yyy` and `zzz`.
     */
-  def until(xxx: ???, yyy: ???)(zzz: ???): ??? = ???
+  def until(singleton: List[CodeTree] => Boolean,
+            combine: List[CodeTree] => List[CodeTree])
+           (trees: List[CodeTree]): List[CodeTree] = {
+    if (singleton(trees)) trees
+    else until(singleton, combine)(combine(trees))
+  }
 
   /**
     * This function creates a code tree which is optimal to encode the text `chars`.
